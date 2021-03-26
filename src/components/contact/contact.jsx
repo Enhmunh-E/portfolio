189c0d6ec281cce0abdee5242231584e22c7da4a
enhuush_enhmunh <enhuush0704@gmail.com>
2021-03-26 12:21:35 +0800

    abcd

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -10,12 +10,12 @@ export const Contact = () => {
         <div className="c-default fs-20 pl-30 lh-normal flex flex-col">
             <p className="">Contact me</p>
             <div className="flex flex-row">
-                <input className="my-10 br-none b-primary raleway w-50 mr-10" type="text" id="name" placeholder="Your Name" onChange={(e) => handler(e)}/>
-                <input className="my-10 br-none b-primary raleway w-50 ml-10" type="email" id="email" placeholder="Your Email" onChange={(e) => handler(e)}/>
+                <input className="my-10 br-none b-primary raleway w-50 mr-10 fs-20" type="text" id="name" placeholder="Your Name" onChange={(e) => handler(e)}/>
+                <input className="my-10 br-none b-primary raleway w-50 ml-10 fs-20" type="email" id="email" placeholder="Your Email" onChange={(e) => handler(e)}/>
             </div>
             <div className="flex flex-col">
                 {/* <input className="my-10 br-none b-primary raleway" type="text" placeholder="Your Subject" id="subject" onChange={(e) => handler(e)}/> */}
-                <textarea className="my-10 br-none b-primary raleway message" type="text" placeholder="Your Subject" id="text" onChange={(e) => handler(e)}/>
+                <textarea className="my-10 br-none b-primary raleway message fs-20" type="text" placeholder="Your Subject" id="text" onChange={(e) => handler(e)}/>
             </div>
         </div>
     );
